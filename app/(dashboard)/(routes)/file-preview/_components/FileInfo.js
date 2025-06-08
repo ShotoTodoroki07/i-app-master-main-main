b2e6acc5841abb2440f01c4948a02cd02d24567e
ShotoTodoroki07 <650112418057@gmail.com>
2025-06-09 02:50:57 +0700

    first commit

--- app/(dashboard)/(routes)/file-preview/_components/FileInfo.js
+++ app/(dashboard)/(routes)/file-preview/_components/FileInfo.js
@@ -1,26 +1,28 @@
-import { useEffect, useState } from 'react';
 import Image from 'next/image';
+import React, { useEffect, useState } from 'react'
 
-function FileInfo({ file }) {
-  const [fileType, setFileType] = useState();
-  useEffect(() => {
-    file && setFileType(file?.fileType.split('/')[0]);
-    console.log(fileType);
-  }, [file]);
-  return file && (
-    <div className="text-center border flex justify-center m-4 flex-col items-center p-2 rounded-md border-blue-20">
-      <Image src={fileType === 'image' ? file?.fileUrl : '/file.png'}
+function FileInfo({file}) {
+    const [fileType,setFileType]=useState();
+    useEffect(()=>{
+        file&&setFileType(file?.fileType.split('/')[0]);
+        console.log(fileType);
+    },[file])
+  return file&&(
+    <div className='text-center border
+     flex justify-center m-4 flex-col items-center p-2 rounded-lg
+     border-blue-200'>
+        <Image src={fileType=='image'?file?.fileUrl:'/file.png'}
         width={200}
         height={200}
-        className='h-[200px] rounded-md object-contain'/>
-      <div className="mt-2"> 
-        <h2>{file.fileName}</h2>
-        <h2 className="text-lg font-semibold">{file.fileName}</h2>
-        
-      </div>
+        alt='logo'
+        className='h-[200px] rounded-md object-contain '/>
+        <div className=''>
+            <h2>{file.fileName}</h2>
+            <h2 className='text-gray-400 text-[13px]'>{file.fileType} / {file.fileSize}</h2>
+
+        </div>
     </div>
-  );
+  )
 }
 
-export default FileInfo;
-
+export default FileInfo
--- app/(dashboard)/(routes)/upload/page.js
+++ app/(dashboard)/(routes)/upload/page.js
@@ -14,7 +14,7 @@ function Upload() {
     const [progress, setProgress] = useState(0)
     const router=useRouter();
     const storage=getStorage(app)
-    const db = getFirestore(app);
+    const db = getFirestore(app); 
     const [fileDocId,setFileDocId]=useState();
     const uploadFile=(file)=>{
         const metadata = {
@@ -28,7 +28,13 @@ function Upload() {
             const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
             setProgress(progress);  // อัพเดต progress
             console.log('Upload is ' + progress + '% done');
-            progress==100&&getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        },
+            (error) => {
+            console.error("Upload failed:", error);
+        },
+        () => {
+
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
                 console.log('File available at', downloadURL);
                 saveInfo(file, downloadURL, user);
             });
@@ -36,28 +42,24 @@ function Upload() {
     }
     const saveInfo=async(file,fileUrl,user)=>{
         const docId=generateRandomString().toString();
-        await setDoc(doc(db, "uploadedFile", docId), {
-            fileName:file?.name,
-            fileSize:file?.size,
-            fileType:file?.type,
-            fileUrl:fileUrl,
-            userEmail:user?.primaryEmailAddress.emailAddress,
-            userName:user?.fullName,
-            password:'',
-            id:docId,
-            shortUrl:process.env.NEXT_PUBLIC_BASE_URL+generateRandomString()
-        });
+        const userEmail = user?.primaryEmailAddress?.emailAddress || null;
+        const userName = user?.fullName || "Unknown";
+        
+         const fileData = {
+        fileName: file?.name,
+        fileSize: file?.size,
+        fileType: file?.type,
+        fileUrl: fileUrl,
+        userEmail: userEmail,
+        userName: userName,
+        password: '',
+        id: docId,
+        shortUrl: process.env.NEXT_PUBLIC_BASE_URL + generateRandomString()
+        };
         setFileDocId(docId)
-    }
 
-// useEffect(()=>{
-//   if (uploadCompleted) {
-//     setTimeout(() => {
-//       setUploadCompleted(false);
-//       // window.location.reload(); // ถ้าจำเป็นค่อยเปิด
-//     }, 2000);
-//   }
-// }, [uploadCompleted]);
+    }
+    
 
     useEffect(()=>{
         console.log("Trigger")
